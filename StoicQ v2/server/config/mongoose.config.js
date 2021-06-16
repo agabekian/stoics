698fadd252cad5f2d4500021599268bdca558035
Lolunts <armasconi@gmail.com>
2021-06-16 14:56:22 -0700

    cleaned
    cc

--- StoicQ v2/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose =  require('mongoose')
-mongoose.connect("mongodb://localhost/crmdb",{
-    // crmdb is used by multiple projects
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Established connection to someDB"))
-.catch(err => console.log("Something went wrong while connecting", err))
