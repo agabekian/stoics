6992c2d38c1d3c6b32be3f6bb7f00c5680e8fa05
agabekian <64112736+agabekian@users.noreply.github.com>
2022-04-18 16:44:18 -0700

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
+
 const mongoose =  process.env.MONGODB_URI || require('mongoose')
 
-
-// mongoose.connect("mongodb://localhost/crmdb",{
-//     // crmdb is used by multiple projects
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-=======
-const mongoose =  require('mongoose')
 mongoose.connect(MONGODB_URI || "mongodb://localhost/crmdb",{
     // crmdb is used by multiple projects
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
->>>>>>> cb4f0f2efaecb97ef947ca4c7f87ce0ccb457c4c
+
 .then(() => console.log("Established connection to someDB"))
 .catch(err => console.log("Something went wrong while connecting", err))
