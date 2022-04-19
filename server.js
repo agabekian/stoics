493af4484d61931e25498a08af68afba0818a44c
Armen Agabekian <armasconi@gmail.com>
2022-04-18 21:16:41 -0700

    wdw

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ require('./server/config/mongoose.config')
 
 require('./server/routes/entries.routes')(app)
 
-const port =  process.env.PORT||8000
+const port =  process.env.PORT
 app.listen(port, () => console.log(`Now listening on port ${port}`))
