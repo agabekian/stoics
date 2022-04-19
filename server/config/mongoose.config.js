0dbee3c4ac6c80b45944bb6e092ca4b11565ed1e
Armen Agabekian <armasconi@gmail.com>
2022-04-18 18:59:08 -0700

    rty

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,14 +1,15 @@
 
 const mongoose = require('mongoose')
 
+const url = "mongodb+srv://agabek:Mongo.8@cluster0.ocgyc.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
-mongoose.connect(process.env.NODE_ENV,
+mongoose.connect(url,
 {
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
     // crmdb is used by multiple projects
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true //new add
 })
-
 .then(() => console.log("Established connection to Atlas?"))
 .catch(err => console.log("Something went wrong while connecting", err))
