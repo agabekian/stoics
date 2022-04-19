4efdc7185e1c9f3a371cb0effe2686314f7ca390
Armen Agabekian <armasconi@gmail.com>
2022-04-18 19:58:27 -0700

    sss

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,25 +1,17 @@
 require('dotenv').config() //for heroku 
 
 const mongoose = require('mongoose')
-mongoCFG = {
-    useNewUrlParser: true,
-    ssl: true,
-    replicaSet: 'Cluster0-shard-0',
-    authSource: 'admin',
-    retryWrites: true,
-    useUnifiedTopology: true,
-  }
-const url = process.env.MONGODB_URI
-// const url = "mongodb+srv://agabek:Mongo.8@cluster0.ocgyc.mongodb.net/Cluster0?retryWrites=true&w=majority"
+// const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI||"mongodb://localhost/crmdb"
 
-mongoose.connect(url,mongoCFG)
-// {
+mongoose.connect(url,
+{
     
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
     // crmdb is used by multiple projects
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true //new add
-// }
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true //new add
+})
 .then(() => console.log("Established connection to Atlas?"))
 .catch(err => console.log("Something went wrong while connecting", err))
