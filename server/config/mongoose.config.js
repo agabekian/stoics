21c078c27da2c5592ea93e3bb29525a02fc66f74
Armen Agabekian <armasconi@gmail.com>
2022-04-18 17:29:18 -0700

    ko

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,11 +1,9 @@
+
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD
-const mongoose = require('mongoose')
-
-=======
->>>>>>> 8a62aea51dbeb9a3b9f6c181ca2be8025bda9866
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
+mongoose.connect("mongodb+srv://agabek:Mongo.8@cluster0.ocgyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+{
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
     // crmdb is used by multiple projects
     useNewUrlParser: true,
     useUnifiedTopology: true,
