8a92f0729cb02acd02cd603ded9084a4dfa88f44
Armen Agabekian <armasconi@gmail.com>
2022-04-18 23:23:41 -0700

    sss

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -3,16 +3,16 @@ require('dotenv').config() //for heroku
 const mongoose = require('mongoose')
 // const url = process.env.MONGODB_URI
 
-// const url = process.env.MONGODB_URI||"mongodb://localhost/crmdb"
+const url = process.env.MONGODB_URI||"mongodb://localhost/crmdb"
 
 // var db = mongoose.connection;
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/crmdb",
+mongoose.connect(url,
 {
+    
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
     useNewUrlParser: true,
     // useUnifiedTopology: true,
     // useCreateIndex: true //new add
 })
-mongoose.connecttion.on('connected',()=>{console.log("Mongoose is connected")})
-// .then(() => console.log("Established connection to Atlas?"))
-// .catch(err => console.log("Something went wrong while connecting", err))
+.then(() => console.log("Established connection to Atlas?"))
+.catch(err => console.log("Something went wrong while connecting", err))
