28dfd299fc9fc9e0f1cfa1332e5dcc2666eec88a
agabekian <64112736+agabekian@users.noreply.github.com>
2022-04-18 17:43:17 -0700

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://agabek:MDOTOCHO@cluster0.ocgyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://agabek:Mongo.8@cluster0.ocgyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 {
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crmdb",{
     // crmdb is used by multiple projects
