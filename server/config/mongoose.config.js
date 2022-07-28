96bde9a60f89e5052e5e262573f4c9f87a8e495b
Agabekian <armasconi@gmail.com>
2022-07-28 15:11:13 -0700

    deploying

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,16 +1,8 @@
 require('dotenv').config() //for heroku 
 const path = require("path");
 const mongoose = require('mongoose')
-// const url = process.env.MONGODB_URI
-
-// const url = process.env.MONGODB_URI
-const url = process.env.MONGODB_URI
-
-// var db = mongoose.connection;
-mongoose.connect(url,
-{
     
-// mongoose.connect(process.env.MONGODB_URI || mongodb+srv://agabek:Mongo.8@cluster0.drmmf.mongodb.net/<quotesDB>?retryWrites=true&w=majority,{
+mongoose.connect("mongodb+srv://agabek:Mongo.8@cluster0.drmmf.mongodb.net/<quotesDB>?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true //new add
