ba7e6dd06150efdf3eee31515d553fa74bc329d6
Agabekian <armasconi@gmail.com>
2022-07-28 18:34:49 -0700

    w

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const app = express(); 
+const app = express();
+const path = require("path");
 
 require('dotenv').config() //for heroku 
 const path = require("path");
@@ -9,6 +10,7 @@ const path = require("path");
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // are Express middleware functions. They are responsible for providing and parsing the request.body data.
 
 
