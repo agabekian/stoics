abfa794ce84a10e5d36dde4e906fb71fa9c44275
Agabekian <armasconi@gmail.com>
2022-07-28 17:27:30 -0700

    [roc

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,7 +2,7 @@ require('dotenv').config() //for heroku
 const path = require("path");
 const mongoose = require('mongoose')
     
-mongoose.connect("mongodb+srv://agabek:Mongo.8@cluster0.drmmf.mongodb.net/quotesDB?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://agabek:Mongo.8@cluster0.drmmf.mongodb.net/<quotesDB>?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true //new add
