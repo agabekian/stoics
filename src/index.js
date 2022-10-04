c4863bb75af6ab702d84fc9e0263ac9eee53a483
Agabekian <armasconi@gmail.com>
2022-10-04 11:45:28 -0700

    more css

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import registerServiceWorker from './registerServiceWorker';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <Auth0Provider
