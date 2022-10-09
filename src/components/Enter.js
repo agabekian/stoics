4c36eb7372b4fc94a3b074ba6987d56cd8df59d6
Agabekian <armasconi@gmail.com>
2022-10-09 15:32:41 -0700

    reload on change

--- src/components/Enter.js
+++ src/components/Enter.js
@@ -4,16 +4,21 @@ import axios from 'axios';
 
 const Enter = () => {
     const [one, setOne] = useState();
+    const [author, setAuthor] = useState();
 
     useEffect(() => {
-        const getRandomOne = () => {
+        const getRandomOne = async () => {
             // const qotd = JSON.parse(window.localStorage.getItem("fQuotes"));
             const qotd = JSON.parse(window.localStorage.getItem("quotes"));
             // console.log(qotd.data.content["0"].text, "one");
             // setOne(qotd.data["0"].content["0"].text)
             // console.log(qotd.length);
-            const pick = (Math.floor(Math.random() * qotd.length)).toString();
-            setOne(qotd[pick])
+            // const pick = (Math.floor(Math.random() * qotd.length)).toString();
+            // setOne(qotd[pick])
+            let res = await axios.get("https://stoic-server.herokuapp.com/random");
+            console.log(res.data["0"].body);
+            setOne(res.data["0"].body)
+            setAuthor(res.data["0"].author)
         }
         getRandomOne();
     }, [])
@@ -22,8 +27,8 @@ const Enter = () => {
         <>
             <img style={{ opacity: "0.7" }} src={require('../images/logoA.png')}></img>
             {/* <p>stoic dude in marble</p> */}
-            <h1 style={{ fontFamily: "cinzel", textDecoration: "none", color: "grey" }}>{one.text}</h1>
-            <p style={{ fontFamily: "cinzel", textDecoration: "none", color: "black" }}>{one.author}</p>
+            <h1 style={{ fontFamily: "cinzel", textDecoration: "none", color: "grey" }}>{one}</h1>
+            <p style={{ fontFamily: "cinzel", textDecoration: "none", color: "black" }}>{author}</p>
         </>
 
     );
